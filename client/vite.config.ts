8f4d48f32f359207047771baa2e83f8a6a759aa4
wu-wilson <wilson.cui.wu@gmail.com>
2025-05-24 18:22:54 -0500

    added proxy for firebase auth

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,25 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { readFileSync } from "fs";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    https: {
-      key: readFileSync(path.join(__dirname, "../certs/localhost-key.pem")),
-      cert: readFileSync(path.join(__dirname, "../certs/localhost.pem")),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    server: {
+      https: {
+        key: readFileSync(path.join(__dirname, "../certs/localhost-key.pem")),
+        cert: readFileSync(path.join(__dirname, "../certs/localhost.pem")),
+      },
+      proxy: {
+        "/__/auth": {
+          target: `https://${env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN}`,
+          changeOrigin: true,
+          secure: true,
+        },
+      },
     },
-  },
+  };
 });
