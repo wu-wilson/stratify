3207b515032b6b3088c507788103d56b720d517f
wu-wilson <wilson.cui.wu@gmail.com>
2025-05-24 17:17:02 -0500

    updated certs using mkcert instead of openssl

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from "vite";
+import { readFileSync } from "fs";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key: readFileSync(path.join(__dirname, "../certs/localhost-key.pem")),
+      cert: readFileSync(path.join(__dirname, "../certs/localhost.pem")),
+    },
+  },
 });
