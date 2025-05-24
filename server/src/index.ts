39ab958582aba20b3ee2053805313c1be3bae5a9
wu-wilson <wilson.cui.wu@gmail.com>
2025-05-24 17:58:14 -0500

    set up backend server and db connection

---
+++ server/src/index.ts
@@ -0,0 +1,44 @@
+import { json as bodyParser } from "body-parser";
+import { Pool } from "pg";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import https from "https";
+import fs from "fs";
+import path from "path";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser());
+
+export const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB,
+  max: 20,
+});
+
+const testDbConnection = async () => {
+  try {
+    const client = await pool.connect();
+    console.log("database connected...");
+    client.release();
+  } catch (err) {
+    console.error("error acquiring client", err);
+    process.exit(1);
+  }
+};
+
+testDbConnection();
+
+const options = {
+  key: fs.readFileSync(path.join(__dirname, "../../certs/localhost-key.pem")),
+  cert: fs.readFileSync(path.join(__dirname, "../../certs/localhost.pem")),
+};
+
+https.createServer(options, app).listen(process.env.PORT, () => {
+  console.log(`listening on port ${process.env.PORT}...`);
+});
