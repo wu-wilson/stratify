f8036dd236b0d8286a3b6705171b9faf754087f1
wu-wilson <wilson.cui.wu@gmail.com>
2025-05-29 18:45:32 -0500

    created logo component

---
+++ client/src/components/logo/Logo.tsx
@@ -0,0 +1,27 @@
+import { FiLayers } from "react-icons/fi";
+import styles from "./Logo.module.scss";
+
+const Logo = ({
+  scale = 1,
+  text = true,
+}: {
+  scale?: number;
+  text?: boolean;
+}) => {
+  const fontSize = `${1 * scale}rem`;
+  const iconSize = `${1.4 * scale}rem`;
+  const spacing = `${0.3 * scale}rem`;
+
+  return (
+    <div className={styles["container"]} style={{ fontSize }}>
+      <FiLayers className={styles["icon"]} style={{ fontSize: iconSize }} />
+      {text && (
+        <span className={styles["text"]} style={{ marginLeft: spacing }}>
+          Stratify
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default Logo;
--- client/src/pages/dashboard/Dashboard.tsx
+++ client/src/pages/dashboard/Dashboard.tsx
@@ -1,7 +1,11 @@
 import styles from "./Dashboard.module.scss";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className={styles["container"]}>
+      <div className={styles["sidebar"]}></div>
+    </div>
+  );
 };
 
 export default Dashboard;
