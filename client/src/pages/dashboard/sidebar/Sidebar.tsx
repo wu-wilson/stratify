84fc6bf2205c265d6ea8d5a3ebb936012b48677c
wu-wilson <wilson.cui.wu@gmail.com>
2025-06-12 00:36:36 -0500

    removed temp projects

--- client/src/pages/dashboard/sidebar/Sidebar.tsx
+++ client/src/pages/dashboard/sidebar/Sidebar.tsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
 import styles from "./Sidebar.module.scss";
-import Tooltip from "../../../components/tooltip/Tooltip";
 
 const Sidebar = ({
   project,
@@ -20,11 +19,6 @@ const Sidebar = ({
     >
       <Header expanded={expanded} setExpanded={setExpanded} />
       {project}
-      <div style={{ height: "100%", overflow: "scroll" }}>
-        {Array.from({ length: 100 }).map((_n, i) => (
-          <Tooltip content={`test ${i}`}>test {i}</Tooltip>
-        ))}
-      </div>
       <Footer expanded={expanded} />
     </div>
   );
