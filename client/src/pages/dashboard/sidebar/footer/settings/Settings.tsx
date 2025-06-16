b4996647171b9ef27a466f2c13ea964e34fcd49f
wu-wilson <wilson.cui.wu@gmail.com>
2025-06-16 12:36:36 -0500

    dynamically updated settings container height

--- client/src/pages/dashboard/sidebar/footer/settings/Settings.tsx
+++ client/src/pages/dashboard/sidebar/footer/settings/Settings.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { type View } from "./types";
 import Root from "./views/root/Root";
 import Name from "./views/name/Name";
@@ -7,8 +7,22 @@ import styles from "./Settings.module.scss";
 const Settings = () => {
   const [view, setView] = useState<View>("root");
 
+  const [height, setHeight] = useState<number | null>(null);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (view === "root" && ref.current) {
+      const height = ref.current.offsetHeight;
+      setHeight(height);
+    }
+  }, [view]);
+
   return (
-    <div className={styles.container}>
+    <div
+      className={styles.container}
+      ref={ref}
+      style={{ height: height ? `${height}px` : undefined }}
+    >
       {view === "root" && <Root setView={setView} />}
       {view === "name" && <Name setView={setView} />}
     </div>
