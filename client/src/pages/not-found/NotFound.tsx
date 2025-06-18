757fa983325b4ac97e398d544f82c872c6c3812e
wu-wilson <wilson.cui.wu@gmail.com>
2025-06-18 15:14:08 -0500

    updated not found page styling

--- client/src/pages/not-found/NotFound.tsx
+++ client/src/pages/not-found/NotFound.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router-dom";
 import { RiArrowLeftDoubleFill, RiHome2Line } from "react-icons/ri";
-import { TbError404 } from "react-icons/tb";
 import styles from "./NotFound.module.scss";
 
 const NotFound = () => {
@@ -13,11 +12,10 @@ const NotFound = () => {
   return (
     <div className={styles.container}>
       <div className={styles.card}>
-        <span className={styles.title}>
-          <TbError404 className={styles.icon} /> Not Found
-        </span>
+        <span className={styles.title}>404</span>
+        <span className={styles.subtitle}>Not Found</span>
         <span className={styles.subtext}>
-          Sorry, we couldn't find the page you're looking for.
+          Sorry, the page you're looking for does not exist or has been moved.
         </span>
         <button className={styles.home} onClick={redirect}>
           <RiArrowLeftDoubleFill /> Home
