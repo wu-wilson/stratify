2bc96ec228890b7ab2c1b1a70f3c02c0fe4337bc
wu-wilson <wilson.cui.wu@gmail.com>
2025-06-19 17:48:35 -0500

    fixed main

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ThemeProvider } from "./contexts/theme/ThemeProvider.tsx";
-import { AuthProvider } from "./contexts/auth/AuthProvider.tsx";
+import { ThemeProvider } from "./contexts/theme/ThemeProvider";
+import { AuthProvider } from "./contexts/auth/AuthProvider";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App.tsx";
+import App from "./App";
 import "./styles/main.scss";
 
 createRoot(document.getElementById("root")!).render(
