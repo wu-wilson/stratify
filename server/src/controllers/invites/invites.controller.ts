61ecabe584464e841700ae606854a8da7c0f1a1f
wu-wilson <wilson.cui.wu@gmail.com>
2025-08-03 21:48:58 -0500

    added validation to accept invite endpoint

--- server/src/controllers/invites/invites.controller.ts
+++ server/src/controllers/invites/invites.controller.ts
@@ -27,49 +27,6 @@ export const getInvite = async (req: Request, res: Response) => {
   }
 };
 
-export const acceptInvite = async (req: Request, res: Response) => {
-  const { member_id, project_id, token } = req.body;
-
-  if (!member_id || !project_id || !token) {
-    res.status(400).json({ error: "id, project_id, and token are required" });
-    return;
-  }
-
-  try {
-    await pool.query("BEGIN");
-
-    await pool.query(
-      `UPDATE invites
-       SET uses = uses + 1
-       WHERE token = $1`,
-      [token]
-    );
-
-    const {
-      rows: [addedMember],
-    } = await pool.query(
-      `INSERT INTO members (id, project_id, role)
-       VALUES ($1, $2, $3)
-       RETURNING *`,
-      [member_id, project_id, "member"]
-    );
-
-    const user = await admin.auth().getUser(member_id);
-    addedMember.name = user.displayName;
-
-    res.status(201).json({
-      message: "Invite accepted successfully",
-      added: addedMember,
-    });
-
-    await pool.query("COMMIT");
-  } catch (error) {
-    await pool.query("ROLLBACK");
-    console.error("Error adding member:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-};
-
 export const createInvite = async (req: Request, res: Response) => {
   const { project_id, max_uses } = req.body;
 
@@ -137,6 +94,64 @@ export const updateInviteStatus = async (req: Request, res: Response) => {
     });
   } catch (error) {
     console.error("Error pausing invite:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const acceptInvite = async (req: Request, res: Response) => {
+  const { member_id, project_id, token } = req.body;
+
+  if (!member_id || !project_id || !token) {
+    res.status(400).json({ error: "id, project_id, and token are required" });
+    return;
+  }
+
+  try {
+    await pool.query("BEGIN");
+
+    const {
+      rows: [invite],
+    } = await pool.query(
+      `SELECT * FROM invites
+       WHERE token = $1 AND project_id = $2 AND paused = FALSE AND uses < max_uses
+       FOR UPDATE`,
+      [token, project_id]
+    );
+
+    if (!invite) {
+      await pool.query("ROLLBACK");
+      res.status(400).json({ error: "Invalid invite" });
+      return;
+    }
+
+    await pool.query(
+      `UPDATE invites
+       SET uses = uses + 1
+       WHERE token = $1`,
+      [token]
+    );
+
+    const {
+      rows: [addedMember],
+    } = await pool.query(
+      `INSERT INTO members (id, project_id, role)
+       VALUES ($1, $2, $3)
+       RETURNING *`,
+      [member_id, project_id, "member"]
+    );
+
+    const user = await admin.auth().getUser(member_id);
+    addedMember.name = user.displayName;
+
+    res.status(201).json({
+      message: "Invite accepted successfully",
+      added: addedMember,
+    });
+
+    await pool.query("COMMIT");
+  } catch (error) {
+    await pool.query("ROLLBACK");
+    console.error("Error adding member:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
