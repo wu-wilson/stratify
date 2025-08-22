251b7c8d8fae7335ad58b950e91dd78c23a51846
wu-wilson <wilson.cui.wu@gmail.com>
2025-08-22 16:28:19 -0500

    added snackbar styles

--- client/src/pages/dashboard/project-views/statuses/Statuses.tsx
+++ client/src/pages/dashboard/project-views/statuses/Statuses.tsx
@@ -1,7 +1,20 @@
 import { useSnackbar } from "../../../../hooks/useSnackbar";
+// import styles from "./Statuses.module.scss";
 
 const Statuses = () => {
-  return <div>Statuses</div>;
+  const { pushMessage } = useSnackbar();
+
+  return (
+    <div>
+      <button
+        onClick={() =>
+          pushMessage({ type: "error", message: "Testing Snackbar" })
+        }
+      >
+        Click Me
+      </button>
+    </div>
+  );
 };
 
 export default Statuses;
